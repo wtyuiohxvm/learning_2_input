7e515eccf4d27abc25c14af506148790b6213744
wtyuiohxvm <41725594+wtyuiohxvm@users.noreply.github.com>
2021-08-16 14:45:43 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 input.onGesture(Gesture.TiltLeft, function () {
     basic.showString("left")
 })
+// 
 input.onButtonPressed(Button.A, function () {
     count += 1
     basic.showNumber(count)
@@ -12,9 +13,21 @@ input.onButtonPressed(Button.B, function () {
     dice = randint(1, 6)
     basic.showNumber(dice)
 })
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showIcon(IconNames.Diamond)
+})
 input.onGesture(Gesture.TiltRight, function () {
     basic.showString("right")
 })
+let test1 = false
 let dice = 0
 let count = 0
 basic.showNumber(count)
+basic.forever(function () {
+    test1 = input.buttonIsPressed(Button.A)
+    if (test1) {
+        basic.showIcon(IconNames.Heart)
+    } else {
+        basic.clearScreen()
+    }
+})
